c2f729bc824ce381b3656a3467fceca0b223bac4
Rowan Elsworth 233759 <233759@student.nottinghamcollege.ac.uk>
2022-10-10 12:28:32 +0100

    added in turns and an array

--- script.js
+++ script.js
@@ -5,18 +5,34 @@ const startBtn = document.getElementById("start-button")
 // startBtn.addEventListener("click", start)
 
 let clicked = []
+var turn = "X"
+
 
 // for each cell click, change text in cell
 $(document).ready(function() {
     $(".cell").each(function(index) {
         $(this).click(function() {
-            $(this).text("O");
-            console.log($(this).attr("index"));
-            clicked.push($(this).attr("index"));
-            $("#debug").text(clicked);
+            let x = $(this).attr("index");
+            if (clicked.includes(x)) {
+                // re run the turn
+            } else {
+                $(this).text(turn);
+                console.log($(this).attr("index"));
+                clicked.push($(this).attr("index"));
+                $("#debug").text(clicked);
+                changeTurn();
+            }
         })
     }) 
 });
+
+function changeTurn() {
+    if (turn == "X") {
+        turn = "O";
+    } else {
+        turn = "X"
+    }
+}
 
 // if clicked don't click again !
 
