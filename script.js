0ce21af9cae774c9b6b9f2d45bf5fba50c94424e
Rowan Elsworth 233759 <233759@student.nottinghamcollege.ac.uk>
2022-10-10 14:40:30 +0100

    added psuedocode and inplay/draw p element

--- script.js
+++ script.js
@@ -4,34 +4,47 @@ const startBtn = $("#start-button")
 
 let clicked = [];
 var turn = "X"
-
+let gameRunning = true;
 
 // for each cell click, change text in cell
-$(document).ready(function() {
-    $(".cell").each(function(index) {
-        $(this).click(function() {
-            let x = $(this).attr("index");
-            if (clicked.includes(x)) {
-                // re run the turn
-            } else {
-                $(this).text(turn);
-                console.log($(this).attr("index"));
-                clicked.push($(this).attr("index"));
-                $("#debug").text(clicked);
-                changeTurn();
-            }
-        })
-    }) 
-});
+if (gameRunning == true) {
+    $(document).ready(function() {
+        $(".cell").each(function(index) {
+            $(this).click(function() {
+                let x = $(this).attr("index");
+                if (clicked.includes(x)) {
+                    // re run the turn
+                } else {
+                    $(this).text(turn);
+                    console.log($(this).attr("index"));
+                    clicked.push($(this).attr("index"));
+                    $("#debug").text(clicked);
+                    ChangeTurn();
+                }
+                if (clicked.length == 9) {
+                    $("#end").text("Draw")
+                }
+            })
+        }) 
+    });
+}
 
-function changeTurn() {
+// draw check
+if (clicked.length == 9) {
+    $("#end").text("Draw");
+}
+
+// changes the turn
+function ChangeTurn() {
     if (turn == "X") {
         turn = "O";
     } else {
         turn = "X"
     }
+    $("#turn").text(`${turn}'s turn`);
 }
 
+// restarts the game
 $(document).ready(function() {
     $("#start-button").click(function() {
         clicked = [];
@@ -39,5 +52,14 @@ $(document).ready(function() {
             $(this).text("") 
         });
         $("#debug").text(clicked);
+        $("#end").text("In play");
     });
 });
+
+// initialise
+// on button click, add the turn into the cell
+// put that cell into the checked list
+// win conditions
+// draw conditions
+// display win / draw
+// restart game
